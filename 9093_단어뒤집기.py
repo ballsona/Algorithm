dfa0942231feff5474998e9de62d8b2141774c76
sona <ballsona0925@naver.com>
2021-08-04 17:18:32 +0900

    210804

---
+++ 9093_단어뒤집기.py
@@ -0,0 +1,12 @@
+import sys
+
+t = int(input())
+for i in range(t):
+    #문장 입력받기
+    case = sys.stdin.readline().split()
+    #단어마다 reverse 하기
+    for i in range(len(case)) :
+        case[i] = ''.join(reversed(case[i]))
+    #리스트 -> 문자열
+    result = " ".join(case)
+    print(result)

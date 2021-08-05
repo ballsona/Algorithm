d1be64e1c753a8fd95d06716da4f47b8c6af0414
sona <ballsona0925@naver.com>
2021-08-06 02:51:22 +0900

    210806

---
+++ 10845_큐.py
@@ -0,0 +1,37 @@
+# push X: 정수 X를 큐에 넣는 연산이다.
+# pop: 큐에서 가장 앞에 있는 정수를 빼고, 그 수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# size: 큐에 들어있는 정수의 개수를 출력한다.
+# empty: 큐가 비어있으면 1, 아니면 0을 출력한다.
+# front: 큐의 가장 앞에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+# back: 큐의 가장 뒤에 있는 정수를 출력한다. 만약 큐에 들어있는 정수가 없는 경우에는 -1을 출력한다.
+
+# 파이썬은 queue 모듈이 존재. FIFO
+
+import sys
+
+n= int(sys.stdin.readline())
+queue=[]
+
+for i in range(n):
+    command = sys.stdin.readline().strip().split(' ')
+    if command[0]=='push':
+        queue.append(command[1])
+    elif command[0]=='pop':
+        if len(queue) == 0:
+            print(-1)
+        else :
+            print(queue.pop(0))
+    elif command[0]=='size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        if len(queue) == 0:
+            print(1)
+        else : print(0)
+    elif command[0] == 'front':
+        if len(queue) == 0:
+            print(-1)
+        else : print(queue[0])
+    elif command[0] == 'back':
+        if len(queue) == 0:
+            print(-1)
+        else : print(queue[-1])

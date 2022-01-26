7004ed03d1983b910ff2cd025d3e70f6f6910ec0
ballsona0925 <ballsona0925@naver.com>
2022-01-26 14:58:19 +0900

    Update : 정렬 practice

--- 정렬/퀵정렬.py
+++ 정렬/퀵정렬.py
@@ -1,5 +1,9 @@
-array = [100,345,9,0,3,11,6,2,4,8]
+from turtle import left, right
 
+
+array = [7,5,9,0,3,1,6,2,4,8]
+
+# 방법1. 직관적이지만 구현이 복잡하고 연산이 비교적 오래걸림.
 def quick_sort(array,start,end):
     pivot = start #여기서는 7
     left = start + 1
@@ -9,7 +13,7 @@ def quick_sort(array,start,end):
             return False
         while left <= end and array[left] < array[pivot] : left += 1
         while right > start and array[right] > array[pivot] : right -=1
-        if left < right :
+        if left < right : 
             # 왼쪽에서부터는 pivot보다 큰 걸 찾고, 오른쪽에서부터는 작은 걸 찾는다. 찾으면 둘이 swap
             array[left], array[right] = array[right],array[left]
         else :
@@ -17,7 +21,18 @@ def quick_sort(array,start,end):
             array[right], array[pivot] = array[pivot],array[right]
         quick_sort(array,start,right-1)
         quick_sort(array,right+1,end)
+    return array
 
+# print(quick_sort(array,0,len(array)-1))
 
-quick_sort(array,0,len(array)-1)
-print(array)
+#방법2. 파이썬 다운 코드
+def quick_sort2(array):
+    if len(array) <= 1: 
+        return array
+    pivot = array[0]
+    tail = array[1:]
+    left_side = [x for x in tail if x<pivot]
+    right_side = [x for x in tail if x>pivot]
+    return quick_sort2(left_side) + [pivot] + quick_sort2(right_side)
+
+print(quick_sort2(array))

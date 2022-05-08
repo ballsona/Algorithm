d54359cef2fbc0baf25014d1eaebe62d842388b5
ballsona0925 <ballsona0925@naver.com>
2022-05-08 23:38:08 +0900

    update: 신고결과받기

--- 프로그래머스/신고결과받기.py
+++ 프로그래머스/신고결과받기.py
@@ -24,3 +24,17 @@ def solution(id_list, report, k):
             if reported_count[prob] >= k :
                 answer[id_list.index(reporter)] += 1
     return answer
+
+def good_solution(id_list, report, k):
+    # 유저 명수 만큼 길이의 answer 배열 생성  
+    answer = [0]*len(id_list)
+    # 유저마다 모두 0을 갖는 딕셔너리 생성
+    reports = {x:0 for x in id_list}
+    # set(report)으로 중복 제거 -> 유저가 report 당한만큼 reports 딕셔너리 값으로 넣어준다. {유저: 신고당한횟수 }
+    for r in set(report):
+        reports[r.split()[1]] += 1
+    # 유저가 신고당한 횟수가 k를 넘는다면 신고자를 찾아서 answer에 1추가해준다. 
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1
+    return answer

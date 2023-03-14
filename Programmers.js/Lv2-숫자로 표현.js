9dd5e557ba74a720f1c258a7dce6549254420269
ballsona <ballsona0925@naver.com>
2023-03-14 13:49:27 +0900

    add Programmers code

--- Programmers.js/Lv2-숫자로 표현.js
+++ Programmers.js/Lv2-숫자로 표현.js
@@ -1,17 +1,26 @@
+// 완전 탐색
 function solution(n) {
   let count = 0;
-  for (i = 1; i < n; i++) {
-    let sum = 0;
-    for (j = i; j < n; j++) {
+
+  for (let i = 1; i < n; i++) {
+    let sum = i; // i부터 시작해서 연속 숫자들의 누적 합
+    for (let j = i + 1; j < n; j++) {
       sum += j;
       if (sum === n) {
         count++;
         break;
       }
-      if (sum >= n) {
-        break;
-      }
+      if (sum >= n) break;
     }
   }
   return count + 1;
+}
+
+// 공식을 이용한 풀이
+function solution2(n) {
+  let count = 0;
+  for (i = 1; i <= n; i++) {
+    if (n % i === 0 && i % 2 !== 0) count++;
+  }
+  return count;
 }
